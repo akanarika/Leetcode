fbbfd98b0d74a0948144c49d9cbd98f44500533b
dandandu <ilykn@hotmail.com>
2017-03-29 18:28:14 -0400

    50 319 added

---
+++ 50 Pow(x, n).py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        
+        if n == 0:
+            return 1
+        if n < 0:
+            return 1 / self.myPow(x, -n)
+        if n % 2 == 1:
+            return self.myPow(x, n-1) * x
+        else:
+            return self.myPow(x*x, n/2)
